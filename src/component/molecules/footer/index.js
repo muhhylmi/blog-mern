83a3a81e17d5a0c158cc05163fd3b6d590608099
muhhylmi <hylmi.muh@gmail.com>
2021-02-11 16:17:30 +0700

    menmbahkan component header dan footer dengan nested router

---
+++ src/component/molecules/footer/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.scss";
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="footer-wrapper">
+        <div className="logo-app">Mern</div>
+        <div className="social-wrapper">
+          <img src="" />
+        </div>
+      </div>
+      <div className="copyright-wrapper">
+        <p>copyright@muh_hylmi</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/component/molecules/header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.scss";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <p className="logo-app">Blog</p>
+      <p className="menu-item">Logout</p>
+    </div>
+  );
+};
+
+export default Header;
